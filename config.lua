532563f12284362da173f5698d52890d458ae14f
Godkong <152982471+jshhusxh12@users.noreply.github.com>
2023-12-06 20:34:09 +0900

    Add files via upload

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 config = {}
 
-config.AppId = "" -- DiscordRichPresence AppID 를 넣으세요.
+config.AppId = "" -- DiscordRichPresence AppID 를 넣으세요. ( https://discord.com/developers/applications )
 
-config.PresenceLogo = ""  -- 로고 파일 이름을 넣으세요.
-config.PresenceText = "https://discord.gg/" -- 디스코드 링크를 넣으세요.
+config.PresenceLogo = ""  -- DiscordRichPresence APP 에 있는 로고 파일 이름을 넣어주세요.
+config.PresenceText = "https://discord.gg/" -- 디스코드 서버 링크를 넣으세요. ( EX : https://discord.gg/setm )
